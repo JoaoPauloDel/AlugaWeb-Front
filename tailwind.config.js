e6f31bec84e4c54406f5df695b0857310ede1613
JoaoPauloDel <joaopaulodelvecchio@gmail.com>
2025-11-26 14:31:27 -0300

    Add initial HTML and Tailwind config files
    Created index.html with basic structure and Tailwind CDN integration. Added tailwind.config.js with custom 'laranja' color for future styling.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+
+    theme: {
+        extend: {
+            colors: {
+                laranja: '#E04300'
+            }
+        },
+    },
+    plugins: [],
+}
